1bb894925e4f5cbca74cd6513b0557fa543003c9
Daniel Palacios <danielpalacios530@gmail.com>
2019-07-29 12:28:57 -0500

    edited user edit form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+   before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+   protected
+
+      def configure_permitted_parameters
+         devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :password, :password_confirmation])
+         devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :last_name, :password, :password_confirmation, :current_password])
+      end
 
 
 
