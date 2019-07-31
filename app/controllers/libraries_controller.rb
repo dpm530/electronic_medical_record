e677ce166d946205531ae7281de56c1f2065eeb2
Daniel Palacios <danielpalacios530@gmail.com>
2019-07-31 11:40:52 -0500

    worked on library design

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -28,7 +28,7 @@ class LibrariesController < ApplicationController
 
     respond_to do |format|
       if @library.save
-        format.html { redirect_to @library, notice: 'Library was successfully created.' }
+        format.html { redirect_to libraries_path, notice: 'Library was successfully created.' }
         format.json { render :show, status: :created, location: @library }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class LibrariesController < ApplicationController
   def update
     respond_to do |format|
       if @library.update(library_params)
-        format.html { redirect_to @library, notice: 'Library was successfully updated.' }
+        format.html { redirect_to libraries_path, notice: 'Library was successfully updated.' }
         format.json { render :show, status: :ok, location: @library }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class LibrariesController < ApplicationController
   def destroy
     @library.destroy
     respond_to do |format|
-      format.html { redirect_to libraries_url, notice: 'Library was successfully destroyed.' }
+      format.html { redirect_to libraries_path, notice: 'Library was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- db/migrate/20190723180345_drop_table_patient_contact_information.rb
+++
@@ -1,5 +0,0 @@
-class DropTablePatientContactInformation < ActiveRecord::Migration[5.2]
-  def change
-     drop_table :patient_contact_informations
-  end
-end
