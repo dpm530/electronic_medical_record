5bedca5b4b9b2fda88e2fc259e24978c030f648a
Daniel Palacios <danielpalacios530@gmail.com>
2019-09-01 19:21:07 -0500

    worked on practice statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -14,6 +14,33 @@ class StatisticsController < ApplicationController
       @termination_notes = TerminationNote.all
       @documents = Document.all
       @notes = 0
+      @aventura_consults = 0
+      @doral_consults = 0
+      @plantation_consults = 0
+      @aventura_patients = 0
+      @doral_patients = 0
+      @plantation_patients = 0
+
+      @aventura_staff.each do |a|
+         consults = a.progress_notes.length
+         patients = a.patients.length
+         @aventura_consults += consults
+         @aventura_patients += patients
+      end
+
+      @doral_staff.each do |d|
+         consults = d.progress_notes.length
+         patients = d.patients.length
+         @doral_consults += consults
+         @doral_patients += patients
+      end
+
+      @plantation_staff.each do |p|
+         consults = p.progress_notes.length
+         patients = p.patients.length
+         @plantation_consults += consults
+         @plantation_patients += patients
+      end
 
       if @intake_notes
          @notes += @intake_notes.length
